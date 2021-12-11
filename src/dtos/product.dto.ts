06e4057817a1b018694aa6be245c9943efdd6f9f
enesuludag <enes.uludag@journzie.com>
2021-12-11 12:55:07 +0300

    .

--- src/dtos/product.dto.ts
+++ src/dtos/product.dto.ts
@@ -11,4 +11,5 @@ export class createProductDto {
   public price: number;
   @IsBoolean()
   public isFavorite: boolean;
+  category: any;
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -3,11 +3,15 @@ import { createProductDto } from "@/dtos/product.dto";
 import { HttpException } from "@exceptions/HttpException";
 import { Products } from "@/interfaces/products.interface";
 import ProductsModel from "@/models/products.model";
+import CategoryModel from "@/models/category.model";
 
 import { isEmpty } from "@utils/util";
+import { Category } from "@/interfaces/category.interface";
 
 class ProductsService {
+  public Category = CategoryModel;
   public Products = ProductsModel;
+
 
   public async findProductsById(productsId: string): Promise<Products> {
     if (isEmpty(productsId))
@@ -26,6 +30,9 @@ class ProductsService {
     if (isEmpty(productsData))
       throw new HttpException(400, "You're not productsData");
     const products: Products[] = await this.Products.find();
+    const category: Category = await this.Category.findOne({ id: productsData.category });
+    if (!category)
+    throw new HttpException(400, "Not found category");
     const length: number = products.length;
     productsData.id = length;
     const createProductsData: Products = await this.Products.create(
